a9c01ea1d9256b0c82ec747d24c1394c90805c23
Chris Dorros <chris.dorros@sv.cmu.edu>
2012-11-07 21:43:39 -0800

    added custom debug function. fixed syntax errors. removed alerts(). added firebug console messages

--- chrome/content/geopki.js
+++ chrome/content/geopki.js
@@ -8,36 +8,32 @@ var Geopki = {
 	
 	// Updates the status of the current page 
 	updateStatus: function(win, is_forced){
-		console.log("updateStatus called");
-		alert("updateStatus Called");
+		Geopki.printDebug("updateStatus called");
 	},
 	
 	geopkiListener: {
 		onSecurityChange:    function() {
-       	/*		var uri = null;
-       			try{
-         			uri = window.gBrowser.currentURI;
-         			Pers_debug.d_print("main", "Security change " + uri.spec + "\n");
-         			Perspectives.updateStatus(window,false);
-       			} catch(err){
-         			Pers_debug.d_print("error", "Perspectives had an internal exception: " + err);
-         			if(uri) {
-          				Pers_statusbar.setStatus(uri, Pers_statusbar.STATE_ERROR, 
-						"Perspectives: an internal security change error occurred: " + err); //TODO: localize
-         			}
-       			}
- 
-  		}*/ 
-		console.log("onSecurityChange called"); // DEBUG MESSAGE
-		alert("onSecurityChange Called");
-		Geopki.updateStatus(window,false);
+			Geopki.printDebug("onSecurityChange called"); // DEBUG MESSAGE
+			Geopki.updateStatus(window,false);
 		}
 	},
 	
 	initGeopki: function() {
-		console.log("initGeopki Called"); // DEBUG MESSAGE
-		alert("initGeopki Called");
+		Geopki.printDebug("initGeopki Called"); // DEBUG MESSAGE
 		getBrowser().addProgressListener(Geopki.geopkiListener, Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+	},
+
+	printDebug: function(message)
+	{
+		try{
+			Firebug.Console.log("GeoPKI: " + message);
+		}
+		catch(e){}
+
+		try{
+			console.log("GeoPKI: "+ message);
+		}
+		catch(e){}
 	}
-	
+
 }
